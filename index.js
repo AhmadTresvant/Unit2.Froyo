755fdf584768679dc98570fba7fba04a3975af6a
Ahmad Ali Tresvant <ahmadtresvant@Alis-M1-Pro.local>
2023-10-31 15:00:39 -0400

    made working function inside the object

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+
+const customerOrder = prompt(`hello what flavor froyo would you like?`);
+
+const froyoOrder = {
+  flavors: [`vanilla`, `strawberry`, `coffee`],
+
+ flavorCount: (customerOrder) => {
+  let flavors = [`vanilla`, `strawberry`, `coffee`];
+    for (let i = 0; i < flavors.length; i++) {
+      console.log(flavors[i]);
+    }
+  }
+  
+
+}
+
+console.log(froyoOrder.flavorCount());
